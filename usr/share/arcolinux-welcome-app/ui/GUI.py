6c3bad46f7963733eb1ae197f4d689401fb72d10
arcolinuxz <arcolinuxinfo@gmail.com>
2024-03-24 15:16:07 +0100

    back to false

--- usr/share/arcolinux-welcome-app/ui/GUI.py
+++ usr/share/arcolinux-welcome-app/ui/GUI.py
@@ -8,7 +8,7 @@ from os.path import expanduser
 from ui.Stack import Stack
 from ui.StackSwitcher import StackSwitcher
 
-debug = True
+debug = False
 
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 home = expanduser("~")
